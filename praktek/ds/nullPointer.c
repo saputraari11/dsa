6323ee30cce7db054967565137af0c0779353f52
saputraari11 <saputra_ari_28rpl@student.smktelkom-mlg.sch.id>
2023-03-10 10:30:14 +0700

    feature: linked list leetcode

--- praktek/ds/nullPointer.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int *ptr = NULL;
-    printf("%x\n",ptr); 
-    // null pointer is used for menyimpan data kosong jika tidak ada memory yang valid dan handling error.return nya selalu 0
-    return 0;
-}
